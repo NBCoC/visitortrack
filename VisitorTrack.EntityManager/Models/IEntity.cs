0c9150718961c61364792403af4ba504e7e8e267
Alberto De Pena <ardepena@gmail.com>
2018-02-03 09:37:21 -0600

    REfactoring

--- VisitorTrack.EntityManager/Models/IEntity.cs
+++
@@ -1,7 +0,0 @@
-namespace VisitorTrack.EntityManager.Models
-{
-    public interface IEntity
-    {
-        string Id { get; set; }
-    }
-}
--- VisitorTrack.EntityManager/Models/RoleEnum.cs
+++
@@ -1,9 +0,0 @@
-namespace VisitorTrack.EntityManager.Models
-{
-    public enum RoleEnum
-    {
-        Admin = 1,
-        Editor = 2,
-        Viewer = 3
-    }
-}

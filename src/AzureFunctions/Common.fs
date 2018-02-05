9fd6d146d729ff7b7448a1f556eb717ea544bbfb
Alberto De Pena <ardepena@gmail.com>
2018-02-05 10:56:57 -0600

    Precompiled

---
+++ src/AzureFunctions/Common.fs
@@ -0,0 +1,54 @@
+namespace AzureFunctions
+
+open System
+//open System.Configuration
+open VisitorTrack.EntityManager.DataTypes
+open System.Collections.Generic
+open System.Net.Http
+open Newtonsoft.Json
+
+[<RequireQualifiedAccess>]
+module Constants =
+
+    let [<Literal>] TokenHeader = "VISITOR-TRACK-TOKEN"
+
+
+[<RequireQualifiedAccess>]
+module Settings =
+
+    let private accountKey = Environment.GetEnvironmentVariable("DbAccountKey")
+    let private endpointUrl = Environment.GetEnvironmentVariable("DbEndpointUri")
+    let private databaseId = Environment.GetEnvironmentVariable("DbName")
+
+    let getStorageOptions collectionId = {
+        AccountKey = accountKey
+        EndpointUrl = endpointUrl
+        CollectionId = collectionId
+        DatabaseId = databaseId
+    }
+
+[<AutoOpen>]
+module Extensions =
+
+    type HttpRequestMessage with
+
+        member this.TryGetQueryStringValue (key: string) =
+
+            let queryStringValue (item: KeyValuePair<string, string>) =
+                if String.Compare(item.Key, key, true) = 0 then
+                    Some item.Value
+                else None
+
+            this.GetQueryNameValuePairs()
+            |> Seq.tryPick queryStringValue
+
+        member this.GetDto<'Dto> () = async {
+            let! data = this.Content.ReadAsStringAsync() |> Async.AwaitTask
+
+            let result =
+                if String.IsNullOrEmpty(data) then
+                    None
+                else JsonConvert.DeserializeObject<'Dto>(data) |> Some
+
+            return result
+        }
---
+++ src/AzureFunctions/UserFunctions.fs
@@ -0,0 +1,64 @@
+﻿namespace AzureFunctions
+
+open System
+//open System.Configuration
+open System.Net
+open System.Net.Http
+open Newtonsoft.Json
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Host
+open VisitorTrack.EntityManager
+open VisitorTrack.EntityManager.Dtos
+open VisitorTrack.EntityManager.DataTypes
+
+module CreateUser =
+
+    [<FunctionName("CreateUser")>]
+    let Run([<HttpTrigger>] req: HttpRequestMessage, log: TraceWriter) = 
+        async {
+            log.Info(sprintf "Executing CreateUser func...")
+
+            let defaultPassword = Environment.GetEnvironmentVariable("DefaultPassword")
+            let storageOptions = Settings.getStorageOptions "UserCollection"
+            let! dto = req.GetDto<UpsertUserDto>()
+
+            let getCommand dto =
+                DefaultPassword defaultPassword, dto
+
+            let create () =
+                dto
+                |> Result.ofOption "User DTO payload is required"
+                |> Result.map getCommand
+                |> Result.bind (UserManager.create storageOptions)
+
+            let result =
+                match create () with
+                | Error error -> req.CreateResponse(HttpStatusCode.BadRequest, error)
+                | Ok (EntityId id) -> req.CreateResponse(HttpStatusCode.OK, id)
+
+            return result
+        } |> Async.RunSynchronously
+
+module DeleteUser =
+
+    [<FunctionName("DeleteUser")>]
+    let Run([<HttpTrigger>] req: HttpRequestMessage, log: TraceWriter) = 
+        async {
+            log.Info(sprintf "Executing DeleteUser func...")
+
+            let storageOptions = Settings.getStorageOptions "UserCollection"
+
+            let delete () = 
+                req.TryGetQueryStringValue "id" 
+                |> Option.map EntityId
+                |> Result.ofOption "User ID is required (?id=<userid>)"
+                |> Result.bind (UserManager.delete storageOptions)
+
+            let result =
+                match delete () with
+                | Error error -> req.CreateResponse(HttpStatusCode.BadRequest, error)
+                | Ok _ -> req.CreateResponse(HttpStatusCode.NoContent)
+
+            return result
+                    
+        } |> Async.RunSynchronously

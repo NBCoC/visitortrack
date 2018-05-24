1585a5a5039201cbd9fbfd237b9356643627c4ee
Alberto De Pena <ardepena@gmail.com>
2018-05-24 15:31:14 -0500

    Fixed report view bug

--- src/VisitorTrack.Client/src/pages/home-page.ts
+++ src/VisitorTrack.Client/src/pages/home-page.ts
@@ -1,9 +1,8 @@
 import { VisitorDialog } from './../dialogs/visitor-dialog';
-import { VisitorReportItem, User } from './../core/models';
+import { VisitorReportItem, User, VisitorCheckListItem } from './../core/models';
 import { ComponentAttached, inject } from 'aurelia-framework';
 import { VisitorTrackService } from './../core/visitor-track-service';
 import { DialogService } from 'aurelia-dialog';
-import { RecentVisitorsDialog } from '../dialogs/recent-visitors-dialog';
 import { Router } from 'aurelia-router';
 
 @inject(VisitorTrackService, DialogService, Router)
@@ -12,6 +11,8 @@ export class HomePage implements ComponentAttached {
   private dialogService: DialogService;
   private router: Router;
   public reportData: VisitorReportItem[];
+  public selectedReport: VisitorReportItem;
+  public hasReport: boolean;
   public user: User;
 
   constructor(visitorTrackService: VisitorTrackService, dialogService: DialogService, router: Router) {
@@ -19,12 +20,15 @@ export class HomePage implements ComponentAttached {
     this.dialogService = dialogService;
     this.router = router;
     this.reportData = [];
+    this.selectedReport = {} as VisitorReportItem;
+    this.hasReport = false;
     this.user = {} as User;
   }
 
   public async attached(): Promise<void> {
     this.user = this.visitorTrackService.getSignedUser();
     this.reportData = await this.visitorTrackService.getReport();
+    this.selectReport(new Date().getMonth() + 1);
   }
 
   public newVisitor(): void {
@@ -34,11 +38,16 @@ export class HomePage implements ComponentAttached {
     });
   }
 
-  public viewDetails(monthId: number): void {
+  public selectReport(monthId: number): void {
     const data = this.reportData.filter(item => item.monthId === monthId);
-    this.dialogService.open({ viewModel: RecentVisitorsDialog, model: data }).whenClosed(result => {
-      if (result.wasCancelled) return;
-      this.router.navigate(`#/main/visitor-details/${result.output}`);
-    });
+
+    this.selectedReport = {
+      monthId: monthId,
+      month: data.map(x => x.month)[0],
+      members: data.map(x => x.members).reduce((a, b) => a.concat(b)),
+      visitors: data.map(x => x.visitors).reduce((a, b) => a.concat(b))
+    };
+
+    this.hasReport = this.selectedReport.members.length > 0 || this.selectedReport.visitors.length > 0;
   }
 }
